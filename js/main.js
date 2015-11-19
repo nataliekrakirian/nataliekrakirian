a1f3df786de444b621d2eafcda7bd386fdc33f43
nataliekrakirian <krakiriann@M205-iMac06.local>
2015-11-18 20:52:40 -0800

    Test commit

---
+++ js/main.js
@@ -0,0 +1,106 @@
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+});
+
+$(document).ready(function(){
+    $('.anchor-scroll').on('click', function (e) {
+      e.preventDefault();
+      var target = $(this).attr('href');
+      $('html,body').animate({
+        scrollTop: $(target).offset().top - 50
+      }, 1000);
+    });
+});
+
+$(document).ready(
+  function () {
+  $(".navbar-nav li a").click(function(event) {
+    if ($(this).hasClass("dropdown-element") || $(this).hasClass("dropdown-toggle")) {
+   
+    } else {
+        $(".navbar-collapse").collapse('hide');
+    }
+  });
+  $(".fadeInAmate").fadeInAmate({
+    initialDelay: 250,
+    fadeInSpeed: 900,
+    animationDelay: 300,
+
+    // enable the slide down animation
+    bounce: true
+  });
+});
+
+/*
+console.log('semi-transparent class is not applied');
+$(document).ready(
+  function () {
+    var iOS;
+    if(navigator.userAgent.match(/(iPad|iPhone|iPod)/g)) {
+      iOS = true;
+    } else {
+      iOS = false;
+    }
+    if(iOS && $('.two-thirds-page-margin').css('min-height') == '66.666666666vh'){
+      $('.two-thirds-page-margin').css('min-height', '0');
+    }
+  }
+);*/
+
+/*THINGS TO ASK RYAN ABOUT
+
+Navbar transparency
+Parallax on subheads
+Scaling text on subheads
+Weird nav mobile issues
+Backgrounds on mobile
+Feasability of sideways expanding accordion
+
+*/
+
+(function(e){"use strict";function r(t,n){this.opts=e.extend({handleKeys:!0,scrollEventKeys:[32,33,34,35,36,37,38,39,40]},n);this.$container=t;this.$document=e(document);this.lockToScrollPos=[0,0];this.disable()}var t,n;n=r.prototype;n.disable=function(){var e=this;e.lockToScrollPos=[e.$container.scrollLeft(),e.$container.scrollTop()];e.$container.on("mousewheel.disablescroll DOMMouseScroll.disablescroll touchmove.disablescroll",e._handleWheel);e.$container.on("scroll.disablescroll",function(){e._handleScrollbar.call(e)});e.opts.handleKeys&&e.$document.on("keydown.disablescroll",function(t){e._handleKeydown.call(e,t)})};n.undo=function(){var e=this;e.$container.off(".disablescroll");e.opts.handleKeys&&e.$document.off(".disablescroll")};n._handleWheel=function(e){e.preventDefault()};n._handleScrollbar=function(){this.$container.scrollLeft(this.lockToScrollPos[0]);this.$container.scrollTop(this.lockToScrollPos[1])};n._handleKeydown=function(e){for(var t=0;t<this.opts.scrollEventKeys.length;t++)if(e.keyCode===this.opts.scrollEventKeys[t]){e.preventDefault();return}};e.fn.disablescroll=function(e){!t&&(typeof e=="object"||!e)?t=new r(this,e):t&&t[e]&&t[e].call(t)};window.UserScrollDisabler=r})(jQuery);
+//SOLID JAVASCRIPT
+/*$(document).ready(
+  function () {
+    var $win = $(window);
+    var winH = $win.height()/2;   
+    function checkScroll(){
+      var navActive = false;
+      if($win.scrollTop() < winH){
+      $(".navbar-header").removeClass("semi-transparent");
+      }else if($win.scrollTop() > winH){
+      $(".navbar-header").addClass("semi-transparent");
+      }
+    }//closed check scroll
+
+    $(".navbar").on('show.bs.collapse', function () {
+      console.log('show.bs.collapse');
+      $(".navbar").disablescroll();
+      if($(window).scrollTop() < winH){
+        $(".navbar").addClass("semi-transparent");
+      } else if($(window).scrollTop() > winH){
+        $(".navbar-collapse").addClass("semi-transparent");
+      }
+      //$('.navbar-default .navbar-nav > li > a').css('color', 'rgba(55, 62, 76, 1)');
+    });
+    $('.navbar').on('hide.bs.collapse', function () {
+      console.log('show.bs.collapse');
+      if($(window).scrollTop() < winH){
+        $(".navbar").removeClass("semi-transparent");
+        $(".navbar").disablescroll("undo");
+      } else if($(window).scrollTop() > winH) {
+        $(".navbar-collapse").removeClass("semi-transparent");
+        $(".navbar").disablescroll("undo");
+      }
+      //$('.navbar-default .navbar-nav > li > a').css('color', 'rgba(55, 62, 76, 0)');
+    });
+    
+
+    
+    if($(".navbar").length > 0){
+        $(window).on("scroll load resize", function(){
+            checkScroll();
+        });
+    }
+  }
+);*/
