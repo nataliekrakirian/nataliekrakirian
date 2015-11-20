b9bb9de605ef228fc5d2a115bdc3bb8a1d724db1
Evan Binder <Evan.Binder@woodburyuniversity.edu>
2015-11-19 17:35:08 -0800

    Test push

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ Feasability of sideways expanding accordion
 
 (function(e){"use strict";function r(t,n){this.opts=e.extend({handleKeys:!0,scrollEventKeys:[32,33,34,35,36,37,38,39,40]},n);this.$container=t;this.$document=e(document);this.lockToScrollPos=[0,0];this.disable()}var t,n;n=r.prototype;n.disable=function(){var e=this;e.lockToScrollPos=[e.$container.scrollLeft(),e.$container.scrollTop()];e.$container.on("mousewheel.disablescroll DOMMouseScroll.disablescroll touchmove.disablescroll",e._handleWheel);e.$container.on("scroll.disablescroll",function(){e._handleScrollbar.call(e)});e.opts.handleKeys&&e.$document.on("keydown.disablescroll",function(t){e._handleKeydown.call(e,t)})};n.undo=function(){var e=this;e.$container.off(".disablescroll");e.opts.handleKeys&&e.$document.off(".disablescroll")};n._handleWheel=function(e){e.preventDefault()};n._handleScrollbar=function(){this.$container.scrollLeft(this.lockToScrollPos[0]);this.$container.scrollTop(this.lockToScrollPos[1])};n._handleKeydown=function(e){for(var t=0;t<this.opts.scrollEventKeys.length;t++)if(e.keyCode===this.opts.scrollEventKeys[t]){e.preventDefault();return}};e.fn.disablescroll=function(e){!t&&(typeof e=="object"||!e)?t=new r(this,e):t&&t[e]&&t[e].call(t)};window.UserScrollDisabler=r})(jQuery);
 //SOLID JAVASCRIPT
-/*$(document).ready(
+$(document).ready(
   function () {
     var $win = $(window);
     var winH = $win.height()/2;   
@@ -83,7 +83,7 @@ Feasability of sideways expanding accordion
       }
       //$('.navbar-default .navbar-nav > li > a').css('color', 'rgba(55, 62, 76, 1)');
     });
-    $('.navbar').on('hide.bs.collapse', function () {
+    $('.navbar').on('hidden.bs.collapse', function () {
       console.log('show.bs.collapse');
       if($(window).scrollTop() < winH){
         $(".navbar").removeClass("semi-transparent");
@@ -103,4 +103,4 @@ Feasability of sideways expanding accordion
         });
     }
   }
-);*/
+);
